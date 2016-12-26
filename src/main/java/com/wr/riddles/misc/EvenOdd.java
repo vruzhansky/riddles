fd62b1a6841fef94ee93d844510d699e986b3835
vruzhansky <vruzhansky@gmail.com>
2016-12-26 01:04:54 +0100

    add even odd riddle

---
+++ src/main/java/com/wr/riddles/misc/EvenOdd.java
@@ -0,0 +1,50 @@
+package com.wr.riddles.misc;
+
+/**
+ * <a href="http://www.geeksforgeeks.org/segregate-even-and-odd-numbers/">http://www.geeksforgeeks.org/segregate-even-and-odd-numbers</a>
+ * <p/>
+ * Segregate Even and Odd numbers
+ * Given an array A[], write a function that segregates even and odd numbers. The functions should put all even numbers first,
+ * and then odd numbers.
+ * <p>
+ * Example
+ * <p>
+ * Input  = {12, 34, 45, 9, 8, 90, 3}
+ * Output = {12, 34, 8, 90, 45, 9, 3}
+ * In the output, order of numbers can be changed, i.e., in the above example 34 can come before 12 and 3 can come before 9.
+ * <p>
+ * The problem is very similar to our old post Segregate 0s and 1s in an array, and both of these problems are variation of
+ * famous Dutch national flag problem.
+ * <p>
+ * <p>
+ * Algorithm: segregateEvenOdd()
+ * 1) Initialize two index variables left and right: left = 0,  right = size -1
+ * 2) Keep incrementing left index until we see an odd number.
+ * 3) Keep decrementing right index until we see an even number.
+ * 4) If lef < right then swap arr[left] and arr[right]
+ */
+public class EvenOdd {
+
+    public static int[] evensFirst(int[] numbers) {
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            if (numbers[i] % 2 == 0) {
+                if (numbers[j] % 2 == 1) {
+                    j--;
+                }
+                i++;
+            } else {
+                if (numbers[j] % 2 == 0) {
+                    int k = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = k;
+                    i++;
+                    j--;
+                } else {
+                    j--;
+                }
+            }
+        }
+        return numbers;
+    }
+}
---
+++ src/test/java/com/wr/riddles/misc/EvenOddTest.java
@@ -0,0 +1,23 @@
+package com.wr.riddles.misc;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class EvenOddTest {
+
+    @DataProvider(name = "numbers")
+    public static Object[][] numbers() {
+        return new Object[][]{
+                {new int[]{4, 5, 6, 7}, new int[]{4, 6, 5, 7}},
+                {new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, new int[]{8, 2, 6, 4, 5, 3, 7, 1, 9}},
+                {new int[]{4, 5, 6, 7}, new int[]{4, 6, 5, 7}},
+        };
+    }
+
+    @Test(dataProvider = "numbers")
+    public void testNumbers(int[] input, int[] expected) throws Exception {
+        int[] actual = EvenOdd.evensFirst(input);
+        Assert.assertEquals(actual, expected);
+    }
+}
