351828d284b418144c2c7a665da1fc9f53f5e752
Volodymyr Ruzhanskyi <vruzhansky@gmail.com>
2017-02-04 15:46:11 +0100

    simple map based lfu cache

---
+++ src/main/java/com/wr/riddles/cache/lfu/MapBasedLFUCache.java
@@ -0,0 +1,70 @@
+package com.wr.riddles.cache.lfu;
+
+import com.wr.riddles.cache.Cache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapBasedLFUCache<K, V> implements Cache<K, V> {
+    private final int size;
+    private final Map<K, Entry<V>> cache = new HashMap<>();
+
+    public MapBasedLFUCache(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public void add(K key, V value) {
+        if (cache.size() < size) {
+            if (cache.containsKey(key)) {
+                cache.put(key, new Entry<>(value, cache.get(key).frequency++));
+            } else {
+                cache.put(key, new Entry<>(value));
+            }
+        } else {
+            cache.entrySet()
+                    .stream()
+                    .min((o1, o2) -> o1.getValue().frequency < o2.getValue().frequency ? -1 :
+                            o1.getValue().frequency > o2.getValue().frequency ? +1 : 0)
+                    .ifPresent(entry -> cache.remove(entry.getKey()));
+
+            cache.put(key, new Entry<>(value));
+        }
+    }
+
+    @Override
+    public void remove(K key) {
+        cache.remove(key);
+    }
+
+    @Override
+    public V get(K key) {
+        Entry<V> entry = cache.get(key);
+        return entry != null ? entry.get() : null;
+    }
+
+    private static class Entry<V> implements Comparable<Entry<V>> {
+        private final V value;
+        private int frequency;
+
+        private Entry(V value) {
+            this(value, 0);
+        }
+
+        private Entry(V value, int frequency) {
+            this.value = value;
+            this.frequency = frequency;
+        }
+
+        private V get() {
+            frequency++;
+            return value;
+        }
+
+        @Override
+        public int compareTo(Entry<V> o) {
+            return this.frequency < o.frequency ? -1 :
+                    this.frequency > o.frequency ? 1 : 0;
+        }
+    }
+}
---
+++ src/test/java/com/wr/riddles/cache/lfu/LFUCacheTest.java
@@ -0,0 +1,41 @@
+package com.wr.riddles.cache.lfu;
+
+import com.wr.riddles.cache.Cache;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.testng.Assert.assertNull;
+
+public class LFUCacheTest {
+
+    @DataProvider(name = "cache")
+    public static Object[][] cache() {
+        return new Object[][]{
+                {new MapBasedLFUCache<>(3)},
+
+        };
+    }
+
+    @Test(dataProvider = "cache")
+    public void testGet(Cache<String, String> cache) throws Exception {
+        cache.add("1", "1");
+        cache.add("2", "2");
+        cache.add("3", "3");
+
+        assertThat(cache.get("1"), equalTo("1"));
+        assertThat(cache.get("2"), equalTo("2"));
+        assertThat(cache.get("3"), equalTo("3"));
+
+        cache.get("1");
+        cache.get("2");
+
+        cache.add("4", "4");
+        assertThat(cache.get("1"), equalTo("1"));
+        assertNull(cache.get("3"));
+        assertThat(cache.get("2"), equalTo("2"));
+        assertThat(cache.get("4"), equalTo("4"));
+    }
+
+}
