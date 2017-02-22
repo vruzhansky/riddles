2c2a327992a01e5f8005043434f01f3bcfa07cd9
Volodymyr Ruzhanskyi <vruzhansky@gmail.com>
2017-02-23 00:18:57 +0100

    cost minimizing stub

---
+++ src/main/java/com/wr/riddles/misc/CostMinimizing.java
@@ -0,0 +1,15 @@
+package com.wr.riddles.misc;
+
+/**
+ * A man has to travel for given number of days by bus. He can buy either:
+ * <p>
+ * 1 day ticket for 2$ (valid for 1 day)
+ * 7 days ticket for 7$ (valid for 7 consecutive days)
+ * 30 days ticket for 25$ (valid for 30 consecutive days)
+ * We are given an array, whose elements are the day numbers on which he travels.
+ * <p>
+ * For example, if the given array is {1,3,5,8,9,10}, then he travels on these 6 days.
+ * Now we have to minimize the cost of his travel
+ */
+public class CostMinimizing {
+}
