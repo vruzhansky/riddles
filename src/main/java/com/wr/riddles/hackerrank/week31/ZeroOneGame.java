8f575574cd45e7c263dc5636975deeee6ae4e6d3
Volodymyr Ruzhanskyi <vruzhansky@gmail.com>
2017-04-17 14:35:54 +0200

    zero-one game

---
+++ src/main/java/com/wr/riddles/hackerrank/week31/ZeroOneGame.java
@@ -0,0 +1,38 @@
+package com.wr.riddles.hackerrank.week31;
+
+import java.util.Scanner;
+
+public class ZeroOneGame {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int g = in.nextInt();
+        for (int a0 = 0; a0 < g; a0++) {
+            int n = in.nextInt();
+            int[] sequence = new int[n];
+            for (int i = 0; i < n; i++) {
+                sequence[i] = in.nextInt();
+            }
+            System.out.println(winner(sequence));
+        }
+    }
+
+    private static String winner(int[] seq) {
+        int n = seq.length;
+        int zeroCount = 0, total = 0;
+        for (int i = 0; i < n; i++) {
+            if (seq[i] == 0) {
+                zeroCount++;
+            } else {
+                if (zeroCount > 0 && i < n - 1 && seq[i + 1] == 0) {
+                    zeroCount++;
+                } else {
+                    total += zeroCount > 2 ? zeroCount : 0;
+                    zeroCount = 0;
+                }
+            }
+        }
+        total += zeroCount > 2 ? zeroCount : 0;
+        return total % 2 == 0 ? "Bob" : "Alice";
+    }
+}
