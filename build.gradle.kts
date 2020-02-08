4798d4b0cc058e410129346a317a62bcfb6b6d9c
Volodymyr Ruzhanskyi <vruzhansky@gmail.com>
2020-02-08 13:29:29 +0100

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.wr"
 version = "1.0-SNAPSHOT"
-
 // versions
-val kotlinVersion = "1.2.41"
 val junitVersion = "5.2.0"
 val hamcrestVersion = "1.3"
 
 plugins {
     java
-    kotlin("jvm") version "1.2.41"
+    kotlin("jvm") version "1.3.61"
 }
 
 repositories {
@@ -18,27 +16,22 @@ repositories {
 }
 
 dependencies {
-    compile(kotlin("stdlib-jdk8:$kotlinVersion"))
-    testCompile("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    testCompile("org.junit.jupiter:junit-jupiter-params:$junitVersion")
-    testCompile("org.hamcrest:hamcrest-all:$hamcrestVersion")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testImplementation("org.hamcrest:hamcrest-all:$hamcrestVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
-
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "12"
+    }
+}
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-task<Wrapper>("wrapper") {
-    gradleVersion = "4.7"
-    distributionUrl = "https://services.gradle.org/distributions/gradle-$gradleVersion-all.zip"
 }
